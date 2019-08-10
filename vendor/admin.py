bcd87cc2d51ec2116d66776a2e817a925f3e51f1
Grant Viklund <grant.viklund@gmail.com>
2019-08-10 15:36:29 -0700

    Working out the models.

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from vendor.models import Catalog, SalePrice, Cart, CartItem, Order, OrderItem
+from vendor.models import Catalog, Price, Order, OrderItem
 
 admin.site.register(Catalog)
-admin.site.register(SalePrice)
-admin.site.register(Cart)
-admin.site.register(CartItem)
+admin.site.register(Price)
 admin.site.register(Order)
 admin.site.register(OrderItem)
