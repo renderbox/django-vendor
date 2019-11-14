5817372fe61d9a23bcf900e2d64d58524184a269
Grant Viklund <grant.viklund@gmail.com>
2019-11-14 15:30:57 -0800

    Updated Admin interface to use inlines.

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
-# from vendor.models import Catalog, Price, Invoice, OrderItem
 from vendor.models import Price, Invoice, OrderItem, Offer, Purchase, CustomerProfile, Refund
+
+###############
+# INLINES
+###############
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+class PriceInline(admin.TabularInline):
+    model = Price
+    extra = 1
+
 
 ###############
 # MODEL ADMINS
@@ -10,17 +22,21 @@ from vendor.models import Price, Invoice, OrderItem, Offer, Purchase, CustomerPr
 
 class OfferAdmin(admin.ModelAdmin):
     readonly_fields = ('uuid',)
-   
+    inlines = [
+        PriceInline,
+    ]
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline,
+    ]
 
 ###############
 # REGISTRATION
 ###############
 
-# admin.site.register(Catalog)
 admin.site.register(Offer, OfferAdmin)
-admin.site.register(Price)
-admin.site.register(Invoice)
-admin.site.register(OrderItem)
+admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(Purchase)
 admin.site.register(CustomerProfile)
 admin.site.register(Refund)
