2d16ab9935923444cb4a2f3699a4db3333009588
Grant Viklund <grant.viklund@gmail.com>
2020-08-25 17:09:47 -0700

    Updated the CI, docs and setup.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Django Vendor'
-copyright = '2020, Grant Viklund'
-author = 'Grant Viklund'
+copyright = '2020, Grant Viklund/Whitemoon Labs'
+author = 'Grant Viklund, Roberto Himmelbauer'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
@@ -31,7 +31,14 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
+    'recommonmark',
 ]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,9 +54,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
