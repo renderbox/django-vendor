58521f3b85910396c808dedf4a417f5c5265446b
Grant Viklund <grant.viklund@gmail.com>
2020-08-25 17:32:17 -0700

    Added note to AddressModel fields

--- vendor/models.py
+++ vendor/models.py
@@ -269,6 +269,17 @@ class CustomerProfile(CreateUpdateModelBase):
 
 
 class Address(models.Model):
+    """
+    Potential Format to use.
+
+    Frau                [title]
+    Mag. Maria Muster   [recipient]
+    Gartenweg 8         [address1]
+                        [address2]
+    Rafing              [locality]
+    3741 PULKAU         [postal code]
+    AUSTRIA             [country]
+    """
     name = models.CharField(_("Name"), max_length=80, blank=True)                                           # If there is only a Product and this is blank, the product's name will be used, oterhwise it will default to "Bundle: <product>, <product>""
     profile = models.ForeignKey(CustomerProfile, verbose_name=_("Customer Profile"), null=True, on_delete=models.CASCADE, related_name="addresses")
     address = AddressField()
