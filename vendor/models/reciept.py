5cdbe00206f268cd80a5cba4483a6c136fea74df
Rob <32716015+rhimmelbauer@users.noreply.github.com>
2020-09-04 09:10:47 -0700

    remove RecieptStatusChoices to PurchaseStatus as they where duplicated

--- vendor/models/reciept.py
+++ vendor/models/reciept.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 from .base import CreateUpdateModelBase
+from vendor.models.choice import PurchaseStatus
 
 #####################
 # TAX CLASSIFIER
@@ -13,14 +14,6 @@ class Reciept(CreateUpdateModelBase):
     A link for all the purchases a user has made. Contains subscription start and end date.
     This is generated for each item a user purchases so it can be checked in other code.
     '''
-    class RecieptStatus(models.IntegerChoices):
-        QUEUED = 0, _("Queued") 
-        PROCESSING = 10, _("Processing")
-        EXPIRED = 20, _("Expired")
-        HOLD = 30, _("Hold")
-        CANCELED = 40, _("Canceled") 
-        REFUNDED = 50, _("Refunded") 
-        COMPLETED = 60, _("Completed")
     profile = models.ForeignKey("vendor.CustomerProfile", verbose_name=_("Purchase Profile"), null=True, on_delete=models.CASCADE, related_name="reciepts")
     order_item = models.ForeignKey('vendor.OrderItem', verbose_name=_("Order Item"), on_delete=models.CASCADE, related_name="reciepts")
     product = models.ForeignKey(settings.VENDOR_PRODUCT_MODEL, on_delete=models.CASCADE, related_name="reciepts", blank=True, null=True)           # TODO:  Goal is to make it easier to check to see if a user owns the product. WHAT IF IT IS A BUNDLE
@@ -29,7 +22,7 @@ class Reciept(CreateUpdateModelBase):
     auto_renew = models.BooleanField(_("Auto Renew"), default=False)        # For subscriptions
     vendor_notes = models.TextField()
     transaction = models.CharField(_("Transaction"), max_length=80)
-    status = models.IntegerField(_("Status"), choices=RecieptStatus.choices, default=0)       # Fulfilled, Refund
+    status = models.IntegerField(_("Status"), choices=PurchaseStatus.choices, default=0)       # Fulfilled, Refund
     class Meta:
         verbose_name = _("Reciept")
         verbose_name_plural = _("Reciepts")
