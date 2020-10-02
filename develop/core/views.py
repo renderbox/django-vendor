3c1940a6378d339467835a9e7364de4dc1dd3b9d
Rob <32716015+rhimmelbauer@users.noreply.github.com>
2020-10-02 13:11:41 -0700

    mixin still needs figureing out

--- develop/core/views.py
+++ develop/core/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 
-from vendor.mixins import UserOwnsProductMixin
+# from vendor.mixins import UserOwnsProductMixin
 from vendor.models import Offer
 class VendorIndexView(ListView):
     template_name = "core/index.html"
     model = Offer
 
-class ProductAccessView(UserOwnsProductMixin, TemplateView):
+class ProductAccessView(TemplateView):
     model = Offer
     template_name = "core/product_use.html"
 
