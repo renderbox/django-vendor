382a2ff934e29fe3b05166363fc62fc5c4f4a84d
Grant Viklund <grant.viklund@gmail.com>
2020-11-02 14:37:08 -0800

    Updated to try to handle redirects based on product ownership.

--- vendor/views/mixin.py
+++ vendor/views/mixin.py
@@ -4,35 +4,48 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib import messages
 from django.utils.translation import ugettext as _
+from django.http import Http404
+from django.contrib.sites.models import Site
 
 
-
-
-class ProductRequiredMixin(class):
+class ProductRequiredMixin():
     """
     Checks to see if a user has a required product and if not, redirects them.
     """
 
     product_queryset = None
     product_model = None
-    product_slug_field = "product_slug"
-    product_pk_url_kwarg = "pk"
-    product_slug_url_kwarg = None
     product_redirect = "/"
 
-    # Override the dispatcher on check?
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Verify that the current user owns the product.  Checks on all HTTP methods.
+        """
+        
+        if not self.user_has_product(request.user):
+            return self.handle_no_product()
+        return super().dispatch(request, *args, **kwargs)
 
-    def get_product_slug_field(self):
-        """Get the name of a slug field to be used to look up by slug."""
-        return self.product_slug_field
+    def user_has_product(self):
+        """
+        Check to see if a user has a viable product license based on the get_product_queryset() method.
+
+        TODO: move this to some kind of caching or session variable to avoid hitting the DB on every request.
+        """
+
+        return self.request.user.customer_profile.filter(site=Site.objects.get_current()).get().has_product(self.get_product_queryset())
 
     def get_product_queryset(self):
+        """
+        Method to get the Product(s) needed for the check.  Can be overridden to handle complex queries.
+        """
+
         if self.product_queryset is None:
             if self.product_model:
                 return self.product_model._default_manager.all()
             else:
                 raise ImproperlyConfigured(
-                    "%(cls)s is missing a product QuerySet. Define "
+                    "%(cls)s is missing a Product QuerySet. Define "
                     "%(cls)s.product_model, %(cls)s.product_queryset, or override "
                     "%(cls)s.get_product_queryset()." % {
                         'cls': self.__class__.__name__
@@ -40,35 +53,18 @@ class ProductRequiredMixin(class):
                 )
         return self.product_queryset.all()
 
-    def get_product(self, product_queryset=None):
+    def handle_no_product(self):
         '''
-        Get the product based on the settings.
-        '''
-        if product_queryset is None:
-            product_queryset = self.get_product_queryset()
-
-        pk = self.kwargs.get(self.product_pk_url_kwarg)
-        slug = self.kwargs.get(self.product_slug_url_kwarg)
-
-        if pk is not None:
-            product_queryset = product_queryset.filter(pk=pk)
-
-        # if self.product_slug_keyword:
-        #     return self.product_model.object.get(slug=self.kwargs[self.product_slug_keyword])
-
-        # if self.product_id_keyword:
-        #     return self.product_model.object.get(pk=self.kwargs[self.product_id_keyword])
-
-        return None
-
-    def no_product_path(self):
-        '''
-        The result to be returned if no viable product is found
-        if None is returned, use a 404 response.
+        The result to be returned if no viable product is found owned by the user.
+        If None is returned, default to a 404 response.
 
         This method allows for a dynamic redirect to be used.
         Default is to use the product_redirect path.
         '''
-        messages.info(self.request, _("Product Purchase required."))
 
+        if not self.product_redirect:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                    {'verbose_name': product_queryset.model._meta.verbose_name})
+
+        messages.info(self.request, _("Product Purchase required."))
         return self.product_redirect
