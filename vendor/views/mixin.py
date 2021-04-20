ee784fa93d9a4c45def43b0abc014de6c0a43622
Roberto Himmelbauer <robertoh89@icloud.com>
2021-04-19 17:42:11 -0700

    added back files form master to dev

--- vendor/views/mixin.py
+++ vendor/views/mixin.py
@@ -86,3 +86,64 @@ class ProductRequiredMixin:
 
         messages.info(self.request, _("Product Purchase required."))
         return redirect(self.product_redirect)
+
+
+class SiteOnRequestFilterMixin:
+
+    def get_queryset(self):
+        if hasattr(self.request, 'site'):
+            return self.model.objects.filter(site=get_site_from_request(self.request))
+        return self.model.on_site.all()
+
+
+class TableFilterMixin:
+    """
+    Mixin for List Views that want to add dynamica pagination, ordering and search filters.
+    The mixin expect paginate_by, ordering and search_filter in the request.GET parameter.
+    """
+    paginate_by = None
+    ordering = []
+    search_filter = None
+
+    def search_filter(self, queryset):
+        """
+        Override this funtion in the ListView to filter wanted fields for the model 
+        defined in the ListView
+        """
+        # Override funtion in List View to filter wanted model fields
+        return queryset
+
+    def set_paginate_by(self):
+        """
+        Set the self.paginate_by variable from the Pagination Class in the ListView
+        """
+        if self.request.GET.get('paginate_by'):
+            self.paginate_by = self.request.GET.get('paginate_by')
+
+    def set_ordering(self):
+        """
+        Sets a ordering list that can be intrepreted by the order_by() query method.
+        """
+        self.ordering = []        
+        if self.request.GET.get('ordering', []):
+            self.ordering = [order for order in self.request.GET.get('ordering', []).split(',') if order]
+
+    def get_search_filter_queryset(self, queryset):
+        """
+        If search_filter is in the request.GET parameters it will call search_filter and return
+        a queryset object.
+        """
+        if self.request.GET.get('search_filter'):
+            return self.search_filter(queryset)
+        return queryset
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        self.set_paginate_by()
+
+        self.set_ordering()
+
+        queryset = self.get_search_filter_queryset(queryset)
+
+        return queryset.order_by(*self.ordering)
