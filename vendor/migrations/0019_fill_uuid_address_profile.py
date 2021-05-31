182350678d2cdf2c3299f6b59babbda2c0c8619c
Roberto Himmelbauer <robertoh89@icloud.com>
2021-05-31 11:55:28 -0700

    not really sure if this is the right way to hanle make profiles required

--- vendor/migrations/0019_fill_uuid_address_profile.py
+++ vendor/migrations/0019_fill_uuid_address_profile.py
@@ -4,17 +4,20 @@ from django.db import migrations
 
 import uuid
 
+
 def generate_address_uuid(apps, schema_editor):
     AddressModel = apps.get_model('vendor', 'Address')
     for address in AddressModel.objects.all():
         address.uuid = uuid.uuid4()
         address.save(update_fields=['uuid'])
 
+
 def generate_customer_profile_uuid(apps, schema_editor):
     CustomerProfileModel = apps.get_model('vendor', 'CustomerProfile')
     for customer_profile in CustomerProfileModel.objects.all():
         customer_profile.uuid = uuid.uuid4()
         customer_profile.save(update_fields=['uuid'])
+
 
 class Migration(migrations.Migration):
 
