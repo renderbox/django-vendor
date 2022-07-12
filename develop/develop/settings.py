384d78323e2ff3075322cd433ef0cf061afee3be
Roberto Himmelbauer <robertoh89@gmail.com>
2022-07-12 10:52:38 -0700

    remvoed debugging allowed hosts

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '&1mmk1e%&9p87fvr=&v84u6fx1)$7f&%)*t9#$zfnu$#h#+5v^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rhimmelbauer.com','147.182.240.222', 'localhost']
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
