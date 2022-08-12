9d24c762f920f5bf7686a9fbc087bb214107dd9b
Chris Ogbuehi <chrisog@WhiteMoons-Mac-mini.local>
2022-08-12 11:00:37 -0500

    first run at stripe processor

--- vendor/processors/stripe.py
+++ vendor/processors/stripe.py
@@ -1,104 +1,187 @@
 """
 Payment processor for Stripe.
 """
-from django.conf import settings
-
+import logging
 import stripe
-
+from django.conf import settings
 from .base import PaymentProcessorBase
-from vendor.models.choice import PurchaseStatus
+from vendor.models.choice import (
+    SubscriptionStatus,
+    TransactionTypes,
+    PaymentTypes,
+    TermType,
+    TermDetailUnits,
+    InvoiceStatus,
+    PurchaseStatus
+)
+from vendor.integrations import StripeIntegration
+
+logger = logging.getLogger(__name__)
 
 class StripeProcessor(PaymentProcessorBase):
+    """
+    Implementation of Stripe SDK
+    https://stripe.com/docs/api/authentication?lang=python
+    """
 
-    def processor_setup(self):
-        stripe.api_key = settings.STRIPE_TEST_PUBLIC_KEY    # TODO: This should work, but may not the best way to do this
+    def processor_setup(self, site):
+        self.credentials = StripeIntegration(site)
 
-    def get_checkout_context(self, request=request, context={}):
-        '''
-        The Invoice plus any additional values to include in the payment record.
-        '''
-        context = super().get_checkout_context(request=request, context=context)
-        context['integration_check'] = 'accept_a_payment'
+        if self.credentials.instance:
+            stripe.api_key = self.credentials.instance.private_key
+        elif settings.STRIPE_API_KEY:
+            stripe.api_key = settings.STRIPE_API_KEY
+        else:
+            logger.error("StripeProcessor missing keys in settings: STRIPE_API_KEY")
+            raise ValueError("StripeProcessor missing keys in settings: STRIPE_API_KEY")
 
-        metadata = {}
-        metadata['integration_check'] = 'accept_a_payment'
-        metadata['order_id'] = str(self.invoice.pk)
+        self.init_transaction_types()
 
-        intent = stripe.PaymentIntent.create(
-            amount=int(self.invoice.total * 100),  # Amount in pennies so it can be an int() rather than a float
-            currency=self.invoice.currency,        # "usd"
-            metadata=metadata,
-        )
+    def init_transaction_types(self):
+        self.transaction_types = {
+            TransactionTypes.AUTHORIZE: self.AUTHORIZE,
+            TransactionTypes.CAPTURE: self.CAPTURE,
+            TransactionTypes.REFUND: self.REFUND,
+            TransactionTypes.VOID: self.VOID,
+        }
 
-        context['client_secret'] = intent.client_secret
-        context['pub_key'] = settings.STRIPE_TEST_PUBLIC_KEY
-
-        return context
-
-    def process_payment(self, token):
-        super().process_payment()
-        payment = self.get_payment_model(self.invoice)
-
+    def create_charge(self, source):
+        # TODO do something with result error strings below
+        # TODO integrate vendor.models.Payment
         try:
-            # Use Stripe's library to make requests...
-            # All necessary chage information should come from the invoice
             charge = stripe.Charge.create(
-                amount=amount,
+                amount=self.invoice.total,
                 currency=self.invoice.currency,
-                source=self.transaction_token
+                source=source,
+                customer=self.invoice.profile.user.pk
             )
-
-            payment.transaction = charge['id']
-            payment.success = True
-            payment.status = PurchaseStatus.SETTLED
-
         except stripe.error.CardError as e:
             # Since it's a decline, stripe.error.CardError will be caught
 
-            payment.result = e.json_body
-
-            # err = payment.result.get('error', {})
-            # from django.contrib import messages
-            # print('Status is: %s' % e.http_status)
-            # print('Type is: %s' % e.error.type)
-            # print('Code is: %s' % e.error.code)
-            # # param is '' in this case
-            # print('Param is: %s' % e.error.param)
-            # print('Message is: %s' % e.error.message)
-
+            print('Status is: %s' % e.http_status)
+            print('Code is: %s' % e.code)
+            # param is '' in this case
+            print('Param is: %s' % e.param)
+            print('Message is: %s' % e.user_message)
         except stripe.error.RateLimitError as e:
             # Too many requests made to the API too quickly
-            # pass
-            payment.result = '{"message":"Rate Limit Error"}'
+            result = '{"message":"Rate Limit Error"}'
 
         except stripe.error.InvalidRequestError as e:
             # Invalid parameters were supplied to Stripe's API
-            payment.result = '{"message":"Invalid Parameters"}'
+            result = '{"message":"Invalid Parameters"}'
 
         except stripe.error.AuthenticationError as e:
             # Authentication with Stripe's API failed
             # (maybe you changed API keys recently)
-            payment.result = '{"message":"Not Authenticated"}'
+            result = '{"message":"Not Authenticated"}'
 
         except stripe.error.APIConnectionError as e:
             # Network communication with Stripe failed
-            payment.result = '{"message":"Network Error"}'
+            result = '{"message":"Network Error"}'
 
         except stripe.error.StripeError as e:
             # Display a very generic error to the user, and maybe send
             # yourself an email
-            payment.result = '{"message":"Something Went Wrong.  You were not charged, please try again."}'
+            result = '{"message":"Something Went Wrong.  You were not charged, please try again."}'
 
         except Exception as e:
             # Something else happened, completely unrelated to Stripe
             # TODO: Send email to self
-            payment.result = '{"message":"A serious error has occured.  Our team has been notified."}'
+            result = '{"message":"A serious error has occured.  Our team has been notified."}'
 
-        payment.save()
+        return charge
 
-        # TODO: Set Order Status
-        # invoice.payment = payment
-        # invoice.save()
+    def create_customer(self):
+        # If current user doesnt have a customer id stripe object, create one
+        # TODO do something with result error strings below
+        # TODO save this customer id on user/profile for later use
 
-        return payment
+        try:
+            customer = stripe.Customer.create(
+                name=self.invoice.profile.user.get_full_name(),
+                email=self.invoice.profile.user.email,
+                metadata={'pk': self.invoice.profile.user.pk}
+            )
 
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            result = '{"message":"Rate Limit Error"}'
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            result = '{"message":"Invalid Parameters"}'
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            result = '{"message":"Not Authenticated"}'
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            result = '{"message":"Network Error"}'
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            result = '{"message":"Something Went Wrong.  You were not charged, please try again."}'
+
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            # TODO: Send email to self
+            result = '{"message":"A serious error has occured.  Our team has been notified."}'
+
+        return customer
+
+    def create_payment_intent(self, customer):
+        # Will return client secret value to be returned to the front end to continue processing payment
+        # TODO do something with result error strings below
+
+        try:
+            intent = stripe.PaymentIntent.create(
+                customer=customer['id'],
+                setup_future_usage='off_session',
+                amount=self.invoice.total,
+                currency=self.invoice.currency,
+                automatic_payment_methods={
+                    'enabled': True,
+                },
+            )
+
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+
+            print('Status is: %s' % e.http_status)
+            print('Code is: %s' % e.code)
+            # param is '' in this case
+            print('Param is: %s' % e.param)
+            print('Message is: %s' % e.user_message)
+
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            result = '{"message":"Rate Limit Error"}'
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            result = '{"message":"Invalid Parameters"}'
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            result = '{"message":"Not Authenticated"}'
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            result = '{"message":"Network Error"}'
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            result = '{"message":"Something Went Wrong.  You were not charged, please try again."}'
+
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            # TODO: Send email to self
+            result = '{"message":"A serious error has occured.  Our team has been notified."}'
+
+        return intent.client_secret
