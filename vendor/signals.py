33388747e3fc358abe165f56f2e8538c1ffe415e
Roberto Himmelbauer <robertoh89@gmail.com>
2022-09-12 09:30:59 -0700

    added loggers for create and delete stripe signals

--- vendor/signals.py
+++ vendor/signals.py
@@ -1,16 +1,21 @@
+import logging
+
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+
 from stripe import Customer
 from vendor.models import CustomerProfile, Offer
 from vendor.config import PaymentProcessorSiteConfig, SupportedPaymentProcessor
 from vendor.processors import StripeProcessor
+
+logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=CustomerProfile)
 def stripe_create_customer_signal(sender, instance, created, **kwargs):
     site_configured_processor = PaymentProcessorSiteConfig(instance.site)
 
     if site_configured_processor.get_key_value('payment_processor') != SupportedPaymentProcessor.STRIPE.value:
-        # TODO: logger.error
+        logger.error(f"stripe_create_customer_signal instance: {instance.pk} was not created on stripe for site {instance.site} because site is not configured with Stripe")
         return None
     
     if 'stripe_id' in instance.meta:
@@ -22,21 +27,21 @@ def stripe_create_customer_signal(sender, instance, created, **kwargs):
     query_result = processor.stripe_query_object(processor.stripe.Customer, customer_query)
 
     if not query_result.is_empty:
-        # TODO: log that user can't be created it needs to be synced
+        logger.warning(f"stripe_create_customer_signal instance: {instance.pk} is already on stripe, may need to sync it.")
         return None
    
     customer = processor.build_customer(instance)
 
     instance.meta['stripe_id'] = customer.id
     instance.save()
-    # TODO: logger info
+    logger.success(f"stripe_create_customer_signal instance: {instance.pk} successfully created on Stripe")
 
 @receiver(post_delete, sender=CustomerProfile)
 def stripe_delete_customer_signal(sender, instance, **kwargs):
     site_configured_processor = PaymentProcessorSiteConfig(instance.site)
 
     if site_configured_processor.get_key_value('payment_processor') != SupportedPaymentProcessor.STRIPE.value:
-        # TODO: logger.error
+        logger.error(f"stripe_delete_customer_signal instance: {instance.pk} can't be deleted as this site: {instance.site} is not configured to use Stripe")
         return None
     
     if 'stripe_id' not in instance.meta:
@@ -45,5 +50,5 @@ def stripe_delete_customer_signal(sender, instance, **kwargs):
     processor = StripeProcessor(instance.site)
 
     processor.stripe_delete_object(processor.stripe.Customer, instance.meta['stripe_id'])
-    # TODO: logger info
+    logger.successs(f"stripe_delete_customer_signal instance: {instance.pk} was successfully deleted on Stripe")
     
