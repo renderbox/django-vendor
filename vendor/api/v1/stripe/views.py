66e0ef87c8eedfef2e9b946e22984d8588d8bbbb
Roberto Himmelbauer <robertoh89@gmail.com>
2022-10-26 10:55:18 -0700

    added StripeEvents TextChoice class to add supported Stripe Events to the vendor API

--- vendor/api/v1/stripe/views.py
+++ vendor/api/v1/stripe/views.py
@@ -1,13 +1,20 @@
+import json
 import stripe
 
 from django.conf import settings
 from django.http.response import HttpResponse
+from django.db.models import TextChoices
+from django.utils.translation import gettext as _
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from vendor.integrations import StripeIntegration
 from vendor.utils import get_site_from_request
 
+
+class StripeEvents(TextChoices):
+    INVOICE_PAID = 'invoice.paid', _('Invoice Paid')
+    INVOICE_PAYMENT_FAILED = 'invoice.payment_failed', _('Invoice Payment Failed')
 
 class StripeBaseAPI(View):
 
@@ -25,9 +32,9 @@ class StripeBaseAPI(View):
             credentials = StripeIntegration(site)
 
             if credentials.instance and credentials.instance.private_key:
-                self.event = stripe.Event.construct_from(json.loads(payload), credentials.instance.private_key)
+                self.event = stripe.Event.construct_from(json.loads(self.request.body), credentials.instance.private_key)
             elif settings.STRIPE_PUBLIC_KEY:
-                self.event = stripe.Event.construct_from(json.loads(payload), settings.STRIPE_PUBLIC_KEY)
+                self.event = stripe.Event.construct_from(json.loads(self.request.body), settings.STRIPE_PUBLIC_KEY)
             else:
                 return False
             
@@ -46,4 +53,34 @@ class StripeSubscriptionPayment(StripeBaseAPI):
         if not self.is_valid_post(site):
             return HttpResponse(status=400)
 
+        if self.event.type != StripeEvents.INVOICE_PAID and self.event.data.object.billing_reason != 'subscription_cycle':
+            return HttpResponse(status=400)
+
+        if self.event.data.object.billing_reason != 'subscription_cycle':
+            return HttpResponse(status=400)
+
         return HttpResponse(status=200)
+
+class StripeSubscriptionPaymentFailed(StripeBaseAPI):
+
+    def post(self, request, *args, **kwargs):
+        site = get_site_from_request(request)
+
+        if not self.is_valid_post(site):
+            return HttpResponse(status=400)
+
+        if self.event.type != StripeEvents.INVOICE_PAYMENT_FAILED:
+            return HttpResponse(status=400)
+
+        return HttpResponse(status=200)
+
+
+# invoice.paid:
+## self.event.data.object.subscription
+## self.event.data.object.status = 'paid'
+## self.event.data.object.customer_email
+
+# invoice.payment_succeded
+
+# invoice.draft
+## self.event.data.object.subscription
