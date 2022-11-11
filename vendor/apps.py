1fda85988766bfb6c1228e7139f617fefdda4985
Roberto Himmelbauer <robertoh89@gmail.com>
2022-11-11 10:57:49 -0800

    added folder signals and renamed the signals file to stripe_signals. Now it only import those signals if the stripe processor is set in the env var

--- vendor/apps.py
+++ vendor/apps.py
@@ -1,8 +1,8 @@
 from django.apps import AppConfig
-
-
 class VendorConfig(AppConfig):
     name = 'vendor'
 
     def ready(self):
-        import vendor.signals
+        from vendor.config import VENDOR_PAYMENT_PROCESSOR, SupportedPaymentProcessor
+        if VENDOR_PAYMENT_PROCESSOR == SupportedPaymentProcessor.STRIPE:
+            import vendor.signals.stripe_signals
