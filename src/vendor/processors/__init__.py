c74ab657748b800db23e363d477d9cfd3daa8276
Grant Viklund <grant.viklund@gmail.com>
2023-03-07 20:13:57 -0500

    Renaming the Stripe processor module to match convention

--- src/vendor/processors/__init__.py
+++ src/vendor/processors/__init__.py
@@ -3,11 +3,14 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from vendor.config import PaymentProcessorSiteConfig
 from vendor.processors.authorizenet import AuthorizeNetProcessor
-from vendor.processors.stripe_processor import StripeProcessor, StripeQueryBuilder
+from vendor.processors.stripe import StripeProcessor, StripeQueryBuilder
 from vendor.processors.base import PaymentProcessorBase
 from vendor.processors.dummy import DummyProcessor
 from siteconfigs.models import SiteConfigModel
 
+
 def get_site_payment_processor(site):
     site_processor = PaymentProcessorSiteConfig(site)
-    return import_string(f"vendor.processors.{site_processor.get_key_value('payment_processor')}")
+    return import_string(
+        f"vendor.processors.{site_processor.get_key_value('payment_processor')}"
+    )
