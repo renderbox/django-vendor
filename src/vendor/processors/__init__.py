845f0f54ee3864e6c4796d78d364a85a618e3d79
Roberto Himmelbauer <robertoh89@gmail.com>
2023-05-03 10:54:29 -0700

    removed AuthorizeNet and Stripe imports form the processor/__init__.py file to avoid having to install them both if either are not required

--- src/vendor/processors/__init__.py
+++ src/vendor/processors/__init__.py
@@ -1,12 +1,6 @@
 from django.utils.module_loading import import_string
-from django.core.exceptions import ObjectDoesNotExist
 
 from vendor.config import PaymentProcessorSiteConfig
-from vendor.processors.authorizenet import AuthorizeNetProcessor
-from vendor.processors.stripe_processor import StripeProcessor, StripeQueryBuilder
-from vendor.processors.base import PaymentProcessorBase
-from vendor.processors.dummy import DummyProcessor
-from siteconfigs.models import SiteConfigModel
 
 def get_site_payment_processor(site):
     site_processor = PaymentProcessorSiteConfig(site)
