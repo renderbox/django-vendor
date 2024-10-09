32f3791d57dd1b476912068ae7068b6303213e14
Roberto Himmelbauer <robertoh89@gmail.com>
2024-10-09 15:04:45 -0700

    commented out allauth middelware

--- develop/develop/settings.py
+++ develop/develop/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'develop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
